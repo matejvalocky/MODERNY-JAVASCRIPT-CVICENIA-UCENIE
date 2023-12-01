10a6152b624a7ab74951dc0586e24ba028f85f67
Matej Valocký <matej.valocky@gmail.com>
2023-12-02 00:17:08 +0100

    95. Moderní JavaScript - Scrollování a praktický příklad s tlačítkem Nahoru

--- script.js
+++ script.js
@@ -3333,6 +3333,37 @@
 
 
 
+// // 95. Moderní JavaScript - Scrollování a praktický příklad s tlačítkem Nahoru
+
+// window.addEventListener("scroll", function(){
+//     let scrolled = window.scrollY
+//     let scrollable = document.documentElement.scrollHeight - window.innerHeight
+    
+//     // if(Math.ceil(scrolled) === scrollable){
+//     //     let toTop = document.querySelector(".top-page")
+//     //     toTop.style.display = "block"
+
+//     //     toTop.addEventListener("click", function(){
+//     //         toTop.style.display = "none"
+//     //     })
+//     // }
+
+//     if(Math.ceil(scrolled) > 500){
+//         let toTop = document.querySelector(".top-page")
+//         toTop.style.display = "block"
+
+//         toTop.addEventListener("click", function(){
+//             toTop.style.display = "none"
+//         })
+//     }
+    
+// })
+
+
+
+
+
+
 
 
 
