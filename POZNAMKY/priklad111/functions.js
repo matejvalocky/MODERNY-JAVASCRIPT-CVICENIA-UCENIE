54b7a662950a789a2430fe7b7c4a2507dcba4a5c
Matej Valocký <matej.valocky@gmail.com>
2024-01-29 23:07:29 +0100

    113. Moderní JavaScript - Miniprojekt vymazání položky Formulář a funkce načtení dat z logalStorage

--- POZNAMKY/priklad111/functions.js
+++ POZNAMKY/priklad111/functions.js
@@ -1,3 +1,18 @@
 // 112. Moderní JavaScript - Miniprojekt vymazání položky Založení projektu
+// 113. Moderní JavaScript - Miniprojekt vymazání položky Formulář a funkce načtení dat z logalStorage
 
 
+/*
+    funkcia načítajúca dáta v localStorage;
+    ošetriť, pokiaľ dáta v localStorage nie sú
+*/
+
+const getSavedNames = function(){
+    const myNames = localStorage.getItem("names")
+
+    if(myNames !== null){
+        return JSON.parse(myNames)
+    } else {
+        return []
+    }
+}
--- POZNAMKY/priklad111/script.js
+++ POZNAMKY/priklad111/script.js
@@ -1,2 +1,17 @@
 // 112. Moderní JavaScript - Miniprojekt vymazání položky Založení projektu
+// 113. Moderní JavaScript - Miniprojekt vymazání položky Formulář a funkce načtení dat z logalStorage
 
+const names = getSavedNames()
+
+
+let myForm = document.querySelector("#test-form")
+myForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    names.push({ // push =  pridanie záznamu na koniec
+        id: "",
+        firstName: event.target.elements.firstName.value
+    })
+
+
+})
