74250e94b25815de0743ba9154af445cfcd39f44
Matej Valocky <matej.valocky@gmail.com>
2024-10-07 21:10:50 +0200

    115. Moderní JavaScript - Miniprojekt vymazání položky: Generujeme ID jména pomocí UUIDv4

--- POZNAMKY/priklad111-druhy/script.js
+++ POZNAMKY/priklad111-druhy/script.js
@@ -1,19 +1,18 @@
-const names = getSavedNames() // do konštanty names mi ulož to, čo mi 
+const names = getSavedNames(); // do konštanty names mi ulož to, čo mi
 // vráti tá funkcia getSavedNames
 
-let myForm  = document.querySelector("#test-form")
+let myForm = document.querySelector("#test-form");
 
-myForm.addEventListener("submit", function(event){
-    event.preventDefault() // zrusenie refreshu stranky po stlaceni na odoslat
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // zrusenie refreshu stranky po stlaceni na odoslat
 
-    names.push({ // push pridanie hodnoty na koniec poľa 
-        id: "", 
-        firstName: event.target.elements.firstName.value // hodnota políčka
-    })
+  names.push({
+    // push pridanie hodnoty na koniec poľa
+    id: uuidv4(), // generovanie špeciálnych jedinečných identifikátorov
+    firstName: event.target.elements.firstName.value, // hodnota políčka
+  });
 
-    event.target.elements.firstName.value = "" // vymazanie hodnoty políčka po kliknutí na submit
+  event.target.elements.firstName.value = ""; // vymazanie hodnoty políčka po kliknutí na submit
 
-    savedNames(names)
-
-
-})
+  savedNames(names);
+});
